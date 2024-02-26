1d58468cae1338479a1f84ee8c073a4d55d2711e
Sai Teja Kolagani <112805613+saiteja-kolagani@users.noreply.github.com>
2024-02-26 21:00:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,3 +35,65 @@ app.get('/players/', async (request, response) => {
   const playersList = await db.all(getPlayersQuery)
   response.send(playersList)
 })
+
+app.post('/players/', async (request, response) => {
+  const bodyDetails = request.body
+  const {playerName, jerseyNumber, role} = bodyDetails
+  const postPlayerDetailsQuery = `
+    INSERT INTO 
+    cricket_team(player_name, jersey_number, role)
+    VALUES(${player_name},
+       ${jersey_number},
+        ${role}
+    );
+  `
+  const dbResponse = await db.run(postPlayerDetailsQuery)
+  const playerId = dbResponse.lastID
+  reponse.send('Player Added to Team')
+})
+
+app.get('/players/:playerId', async (request, response) => {
+  const {playerId} = request.params
+  const getPlayerQuery = `
+    SELECT 
+    * 
+    FROM 
+    cricket_team
+    WHERE 
+    player_id = ${playerId}
+  `
+  const dbResponse = await db.get(getPlayerQuery)
+  response.send(dbResponse)
+})
+
+app.put('/players/:playerId/', async (request, response) => {
+  const {playerId} = request.params
+  const bodyDetails = request.body
+  const {playerName, jerseyNumber, role} = bodyDetails
+  const updateQuery = `
+    UPDATE 
+    cricket_team
+    SET 
+    player_name = ${playerName},
+    jersey_number = ${jerseyNumber},
+    role = ${role}
+    WHERE 
+    player_id = ${playerId}
+  `
+  const dbResponse = await db.run(updateQuery)
+  response.send('Player Details Updated')
+})
+
+app.delete('players/:playerId', async (request, response) => {
+  const {playerId} = request.params
+  const deletePlayerQuery = `
+    DELETE FROM 
+    cricket_team
+    WHERE 
+    player_id = ${playerId}
+  `
+  const dbResponse = await db.run(deletePlayerQuery)
+  response.send('Player Removed')
+})
+
+module.exports = app
